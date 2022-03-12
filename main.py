45b313fcf5c7b97a358608dd0910c34c2079e285
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-03-12 11:03:16 -0300

    Criar arquivos iniciais

---
+++ main.py
@@ -0,0 +1,25 @@
+# Importa métodos da biblioteca selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Importa a biblioteca time
+import time
+
+# Importa a biblioteca json
+import json
+
+# Importa a bibliotca zipfile
+import zipfile
+
+# Importa um método da biblioteca pathlib
+from pathlib import Path
+
+# Importa a biblioteca pandas com o apelido pd
+import pandas as pd
+
+# Importa a biblioteca locale
+import locale
+
+# Importa a biblioteca yagmail
+import yagmail
